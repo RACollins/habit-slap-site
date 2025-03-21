827f6a473fbf44c85959ad20a01cd29cf2063b4e
RACollins <richardcollins128@gmail.com>
2025-03-21 17:35:59 +0900

    Update site URL in login.py to use the correct production domain

--- pages/login.py
+++ pages/login.py
@@ -12,7 +12,7 @@ from datetime import datetime, timedelta, timezone
 load_dotenv()
 db = DynamoHandler()
 is_dev = os.getenv("IS_DEV")
-site_url = "http://0.0.0.0:5001" if is_dev else "https://habitslap.com"
+site_url = "http://0.0.0.0:5001" if is_dev else "https://www.habitslap.com"
 root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ar = fasthtml.APIRouter()
 
